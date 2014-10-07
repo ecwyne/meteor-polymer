40c8e8117fb725549c507e8bfc2fc0125ebe4521
Eric Wyne <ecwyne@gmail.com>
2014-10-07 09:23:14 -0700

    fixes #12 and #15

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
 	summary: "Add Polymer Base to meteor Project",
-	version: "1.0.1",
+	version: "1.0.2",
 	git: "https://github.com/ecwyne/meteor-polymer.git",
 	name: 'ecwyne:polymer'
 });
